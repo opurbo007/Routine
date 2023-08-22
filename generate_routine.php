e1e6715136703765c5e14a72a45bf47815507d84
opurbo007 <opurboujjol@gmail.com>
2023-08-22 17:56:16 +0600

    22-8-2023

--- generate_routine.php
+++ generate_routine.php
@@ -35,28 +35,42 @@
         $selectedRoom = $selectedRooms[$courseId];
         $selectedTeacher = $selectedTeachers[$courseId];
 
-        // Insert the data into the routine table
-        $insertQuery = "INSERT INTO routine (batch, semester, session, course_id, day, start_time, end_time, room_id, teacher_id)
-        VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)";
+        // Check room and teacher availability
+        $availabilityQuery = "SELECT * FROM routine WHERE day = ? AND start_time < ? AND end_time > ? AND (room_id = ? OR teacher_id = ?)";
+        $availabilityStmt = $connection->prepare($availabilityQuery);
+        $availabilityStmt->bind_param("sssii", $selectedDay, $selectedEndTime, $selectedTime, $selectedRoom, $selectedTeacher);
+        $availabilityStmt->execute();
+        $availabilityResult = $availabilityStmt->get_result();
 
-        $insertStmt = $connection->prepare($insertQuery);
-        $insertStmt->bind_param(
-          "iissssssi",
-          $selectedBatch,
-          $selectedSemester,
-          $selectedSession,
-          $courseId,
-          $selectedDay,
-          $selectedTime,
-          $selectedEndTime,
-          $selectedRoom,
-          $selectedTeacher
-        );
-        $insertStmt->execute();
+        if ($availabilityResult->num_rows > 0) {
+          echo "Error: Room or teacher already assigned during the specified day and time.";
+          // You can also provide additional details about the conflicting course
+        } else {
+          // Insert the data into the routine table
+          $insertQuery = "INSERT INTO routine (batch, semester, session, course_id, day, start_time, end_time, room_id, teacher_id)
+              VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)";
 
 
+          $insertStmt = $connection->prepare($insertQuery);
+          $insertStmt->bind_param(
+            "iissssssi",
+            $selectedBatch,
+            $selectedSemester,
+            $selectedSession,
+            $courseId,
+            $selectedDay,
+            $selectedTime,
+            $selectedEndTime,
+            $selectedRoom,
+            $selectedTeacher
+          );
+          $insertStmt->execute();
+
+
+        }
       }
     }
+
 
     echo "Routine generated successfully!";
 
