0d8cc5a36b668701a47f9395f958f51a5ae1ca1f
opurbo007 <opurboujjol@gmail.com>
2023-09-30 23:37:20 +0600

    30-9-2023

--- src/admin/routine/show/view.php
+++ src/admin/routine/show/view.php
@@ -80,7 +80,7 @@ include("../../../include/adminNavbar.php");
         <!-- <label for="teacher_id">Teacher Name:</label> -->
         <select id="teacher_id" name="teacher_id"
           class="text-gray-700 bg-white hover:bg-gray-100 focus:ring-4 focus:outline-none focus:ring-gray-300 font-medium rounded-lg text-sm px-5 py-2.5">
-          <option value="">Select Teacher</option>
+          <option value="">Select Dept First</option>
         </select>
       </div>
 
@@ -253,7 +253,7 @@ document.addEventListener('DOMContentLoaded', function() {
         });
     } else {
       // If no department is selected, show all teachers
-      teacherDropdown.innerHTML = '<option value="">Select Teacher</option>';
+      teacherDropdown.innerHTML = '<option value="">Select Dept First for Teacher</option>';
     }
   }
 
