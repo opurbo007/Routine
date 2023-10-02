a302974c52fc83c9fa6dd3bc8ae1500586fa0dda
opurbo007 <opurboujjol@gmail.com>
2023-10-03 01:23:18 +0600

    final-3-10-2023

--- src/teacher/routine.php
+++ src/teacher/routine.php
@@ -14,25 +14,28 @@ while ($row = $timeSlotsResult->fetch_assoc()) {
 // Fetch and display the teacher's routine
 if (isset($_SESSION["teacher_id"])) {
   $teacherId = $_SESSION["teacher_id"];
-  $routineQuery = "SELECT day, TIME_FORMAT(start_time, '%h:%i %p') AS start_time, TIME_FORMAT(end_time, '%h:%i %p') AS end_time, course_code, course_name, room_number
-                     FROM routine
-                     INNER JOIN course ON routine.course_id = course.course_id
-                     INNER JOIN room ON routine.room_id = room.room_id
-                     WHERE teacher_id = ?
-                     ORDER BY FIELD(day, 'Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'), start_time";
+  $routineQuery = "SELECT routine.day, TIME_FORMAT(routine.start_time, '%h:%i %p') AS start_time, TIME_FORMAT(routine.end_time, '%h:%i %p') AS end_time, course.course_code, course.course_name, room.room_number, batch.batch_number,semester.semester_name
+                 FROM routine
+                 INNER JOIN course ON routine.course_id = course.course_id
+                 INNER JOIN room ON routine.room_id = room.room_id
+                 INNER JOIN batch ON routine.batch = batch.batch_id
+                 INNER JOIN semester ON routine.semester = semester.semester_id
+                 WHERE routine.teacher_id = ?
+                 ORDER BY FIELD(routine.day, 'Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'), routine.start_time";
+
   $routineStmt = $conn->prepare($routineQuery);
   $routineStmt->bind_param("i", $teacherId);
   $routineStmt->execute();
   $routineResult = $routineStmt->get_result();
   ?>
 
-  <div class="container mx-auto py-8">
-    <h1 class="text-3xl font-bold mb-10 text-center">Class Routine</h1>
-    <table class="table-auto w-full bg-white">
-      <thead>
-        <tr>
-          <th class="px-4 border py-2">Day & Time</th>
-          <?php
+<div class="container mx-auto py-8">
+  <h1 class="text-3xl font-bold mb-10 text-center">Class Routine</h1>
+  <table class="table-auto w-full bg-white">
+    <thead>
+      <tr>
+        <th class="px-4 border py-2">Day & Time</th>
+        <?php
           $columnsToDisplay = [];
 
           foreach ($timeSlots as $timeSlot) {
@@ -52,10 +55,10 @@ if (isset($_SESSION["teacher_id"])) {
             }
           }
           ?>
-        </tr>
-      </thead>
-      <tbody>
-        <?php
+      </tr>
+    </thead>
+    <tbody>
+      <?php
         $days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
 
         foreach ($days as $day) {
@@ -68,7 +71,7 @@ if (isset($_SESSION["teacher_id"])) {
             $routineResult->data_seek(0);
             while ($row = $routineResult->fetch_assoc()) {
               if ($row['day'] == $day && "{$row['start_time']} - {$row['end_time']}" == $timeSlot) {
-                $classes[] = "<div class='text-center'><span class='font-bold'>{$row['course_code']}</span><br> {$row['course_name']}<br><span class='font-bold'>{$row['room_number']} </span> </div>";
+                $classes[] = "<div class='text-center'><span class='font-bold'>{$row['course_code']}</span><br> {$row['course_name']}<br><span class='font-bold'>{$row['room_number']} </span> <br><span>[{$row['batch_number']} ({$row['semester_name']})] </span> </div>";
               }
             }
 
@@ -86,15 +89,15 @@ if (isset($_SESSION["teacher_id"])) {
           echo "</tr>";
         }
         ?>
-      </tbody>
-    </table>
-    <div class="flex justify-center mt-8">
-      <button class="bg-gradient-to-r from-green-600 to-blue-600 text-white font-bold py-2 px-4 rounded-full">
-        <a href="pdf.php?teacher_id=<?php echo $teacherId; ?>">Download</a>
-      </button>
-    </div>
+    </tbody>
+  </table>
+  <div class="flex justify-center mt-8">
+    <button class="bg-gradient-to-r from-green-600 to-blue-600 text-white font-bold py-2 px-4 rounded-full">
+      <a href="pdf.php?teacher_id=<?php echo $teacherId; ?>">Download</a>
+    </button>
+  </div>
 
-    <?php
+  <?php
 } else {
   echo "You are not authorized to view this page.";
 }
